fcbfb169dac809a92e4c234d86283d4568a7f4c4
1Mill <erik.ekberg@outlook.com>
2020-02-16 19:47:01 -0800

    Update index.js

--- src/timer/index.js
+++ src/timer/index.js
@@ -5,10 +5,9 @@ io.adapter(redisAdapter({ host: 'redis-service', port: 6379 }));
 
 // * Emit event to everybody
 setInterval(() => {
-	console.log('...')
 	io.emit('chat message', 'Everybody is getting this message...');
 }, 5000);
 
 io.on('connection', (socket) => {
-
+	console.log(socket)
 })
