8a53ee7deb9854847de79961d71fc961eccf7dab
1Mill <erik.ekberg@outlook.com>
2020-02-16 19:40:27 -0800

    Emit message from different service

--- src/timer/index.js
+++ src/timer/index.js
@@ -1,1 +1,9 @@
-// KEEP
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ host: 'redis-service', port: 6379 }));
+
+setInterval(() => {
+	console.log('...')
+	io.emit('chat message', 'TESTING');
+}, 3000);
