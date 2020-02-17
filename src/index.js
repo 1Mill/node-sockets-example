f55e0c1291621605750f25949272153c64dc32cd
1Mill <erik.ekberg@outlook.com>
2020-02-16 16:21:00 -0800

    Serve HTML file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 
+const HTTP_FILE = __dirname + '/index.html';
 const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello world!</h1>');
+	res.sendFile(HTTP_FILE);
 });
 http.listen(PORT, () => {
 	console.log(`listening on *:${PORT}`);
