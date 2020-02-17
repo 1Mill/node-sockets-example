119b6567b75f20312175e1a0af8e53a8ee5979e6
1Mill <erik.ekberg@outlook.com>
2020-02-16 16:24:28 -0800

    Scaffold socket connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const HTTP_FILE = __dirname + '/index.html';
 const PORT = process.env.PORT;
@@ -7,6 +8,11 @@ const PORT = process.env.PORT;
 app.get('/', (req, res) => {
 	res.sendFile(HTTP_FILE);
 });
+
+io.on('connection', (socket) => {
+	console.log('A user connected');
+})
+
 http.listen(PORT, () => {
 	console.log(`listening on *:${PORT}`);
 });
