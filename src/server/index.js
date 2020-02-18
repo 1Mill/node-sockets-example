1a93d98b628c149322b1f5ca34217ce12df1df02
1Mill <erik.ekberg@outlook.com>
2020-02-17 21:39:04 -0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,12 @@
-// KEEP
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ host: 'redis', port: 6379 }));
+
+io.on('connect', (socket) => {
+	console.log('A user connected');
+
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+});
