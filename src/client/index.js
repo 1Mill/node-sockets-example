4a3364cba7f9e5641cc22c5a78a4ab968dcf6b27
1Mill <erik.ekberg@outlook.com>
2020-02-18 21:13:55 -0800

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,7 @@ const io = require('socket.io')(http);
 const redisAdapter = require('socket.io-redis');
 
 io.adapter(redisAdapter({ host: 'redis', port: 6379 }));
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
